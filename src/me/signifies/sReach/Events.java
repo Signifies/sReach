f0d90c6f4e68a3f93c7c94a74f9618ced6066e59
Signifies <sphinxofpsl@gmail.com>
2018-03-24 14:19:21 -0400

    Idk

--- src/me/signifies/sReach/Events.java
+++ src/me/signifies/sReach/Events.java
@@ -1,13 +1,23 @@
 package me.signifies.sReach;
 
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerInteractAtEntityEvent;
 
 /**
  * Created by Signifies for sReach on 3/24/18/1:48 PM
  */
-public class Events implements Listener
-{
+public class Events implements Listener {
 
     //TODO Listener class for event management.
 
+    @EventHandler
+    public void onDetection(PlayerInteractAtEntityEvent event)
+    {
+        Player p = event.getPlayer();
+
+        
+
+    }
 }
